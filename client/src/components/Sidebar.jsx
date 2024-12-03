e2f5556eaf5a3ea76edbc62e8a475f21c6fc5f63
HADJADJDAOUD <daoudhdj@gmail.com>
2024-12-03 15:05:59 +0100

    fixed some issues

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -211,13 +211,13 @@ const Sidebar = ({
                   fontSize="0.9rem"
                   sx={{ color: theme.palette.secondary[100] }}
                 >
-                  daoud
+                  {user.name}
                 </Typography>
                 <Typography
                   fontSize="0.8rem"
                   sx={{ color: theme.palette.secondary[200] }}
                 >
-                  hadjadj
+                  {user.occupation}
                 </Typography>
               </Box>
               <SettingsOutlined
